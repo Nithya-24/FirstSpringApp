0ec64d0596b46f171db62b4faacc33082d409c44
Your Name <nithyaramadoz@gmail.com>
2022-05-19 09:49:14 +0530

    Added: First Spring App displays Hello

---
+++ src/main/java/com/example/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FirstSpringAppApplication.class, args);
+        System.out.println("Hello from Bridgelabz");
+    }
+
+}
